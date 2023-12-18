5916a22b96fb17726970393995bafbc35fbbfd80
vtu1165 <125785539+vtu1165@users.noreply.github.com>
2023-12-18 12:13:19 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'abh7186@gmail.com';
+$email_form = 'info@yourwebsite.com;
 
 $email_subject = 'New Form Submission';
 
